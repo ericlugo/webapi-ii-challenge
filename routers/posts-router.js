4d01301274deb59fbc2fca93dd8367edc83ff1c7
ericlugo <ericlugo@users.noreply.github.com>
2019-07-29 03:15:12 -0400

    refactoring for code cleanliness, efficiency, and readability

--- routers/posts-router.js
+++ routers/posts-router.js
@@ -1,69 +1,40 @@
 const express = require(`express`);
 const db = require(`../data/db.js`);
+const responseHandler = require(`../snippets/responseHandler.js`);
 
 const router = express.Router();
 
 router.post(``, async (request, response) => {
   const { title, contents } = request.body;
-
-  if (!title || !contents) {
-    response.status(400).json({
-      success: false,
-      error: `Please provide title and contents for the post.`,
-    });
-  } else {
+  if (!title || !contents) responseHandler(response, false, 400, { error: `Please provide title and contents for the post.` });
+  else {
     try {
       const postResponse = await db.insert(request.body);
       const post = await db.findById(postResponse.id);
-      response.status(201).json({
-        success: true,
-        post,
-      });
+      responseHandler(response, true, 201, { post });
     } catch {
-      response.status(500).json({
-        success: false,
-        error: `There was an error while saving the post to the database`,
-      });
+      responseHandler(response, false, 500, { error: `There was an error while saving the post to the database` });
     }
   }
 });
 
 router.post(`/:id/comments`, async (request, response) => {
   const { text } = request.body;
-
-  if (!text) {
-    response.status(400).json({
-      success: false,
-      error: `Please provide text for the comment.`,
-    });
-  } else {
+  if (!text) responseHandler(response, false, 400, { error: `Please provide text for the comment.` });
+  else {
     try {
       const post = await db.findById(request.params.id);
       if (post[0]) {
         try {
           const commentResponse = await db.insertComment({ text, post_id: request.params.id });
           const comment = await db.findCommentById(commentResponse.id);
-          response.status(201).json({
-            success: true,
-            comment,
-          });
+          responseHandler(response, true, 201, { comment: comment[0] });
         } catch {
-          response.status(500).json({
-            success: false,
-            error: `There was an error while saving the comment to the database.`,
-          });
+          responseHandler(response, false, 500, { error: `There was an error while saving the comment to the database.` });
         }
-      } else {
-        response.status(404).json({
-          success: false,
-          error: `The post with the specified ID does not exist.`,
-        });
-      }
+      } else responseHandler(response, false, 404, { error: `The post with the specified ID does not exist.` });
     } catch {
-      response.status(500).json({
-        success: false,
-        error: `The post information could not be retrieved.`,
-      });
+      responseHandler(response, false, 500, { error: `The post information could not be retrieved.` });
     }
   }
 });
@@ -71,15 +42,9 @@ router.post(`/:id/comments`, async (request, response) => {
 router.get(``, async (request, response) => {
   try {
     const posts = await db.find();
-    response.status(200).json({
-      success: true,
-      posts,
-    });
+    responseHandler(response, true, 200, { posts });
   } catch {
-    response.status(500).json({
-      success: false,
-      error: `The posts information could not be retrieved.`,
-    });
+    responseHandler(response, false, 500, { error: `The posts information could not be retrieved.` });
   }
 });
 
@@ -87,19 +52,10 @@ router.get(`/:id`, async (request, response) => {
   try {
     const post = await db.findById(request.params.id);
     post[0]
-      ? response.status(200).json({
-          success: true,
-          post: post[0],
-        })
-      : response.status(404).json({
-          success: false,
-          error: `The post with the specified ID does not exist.`,
-        });
+      ? responseHandler(response, true, 200, { post: post[0] })
+      : responseHandler(response, false, 404, { error: `The post with the specified ID does not exist.` });
   } catch {
-    response.status(500).json({
-      success: false,
-      error: `The post information could not be retrieved.`,
-    });
+    responseHandler(response, false, 500, { error: `The post information could not be retrieved.` });
   }
 });
 
@@ -109,39 +65,20 @@ router.get(`/:id/comments`, async (request, response) => {
     if (post[0])
       try {
         const comments = await db.findPostComments(request.params.id);
-        response.status(200).json({
-          success: true,
-          comments,
-        });
+        responseHandler(response, true, 200, { comments });
       } catch {
-        response.status(500).json({
-          success: false,
-          error: `The comments information could not be retrieved.`,
-        });
+        responseHandler(response, false, 500, { error: `The comments information could not be retrieved.` });
       }
-    else {
-      response.status(404).json({
-        success: false,
-        error: `The post with the specified ID does not exist.`,
-      });
-    }
+    else responseHandler(response, false, 404, { error: `The post with the specified ID does not exist.` });
   } catch {
-    response.status(500).json({
-      success: false,
-      error: `The post information could not be retrieved.`,
-    });
+    responseHandler(response, false, 500, { error: `The post information could not be retrieved.` });
   }
 });
 
 router.put(`/:id`, async (request, response) => {
   const { title, contents } = request.body;
-
-  if (!title && !contents) {
-    response.status(400).json({
-      success: false,
-      error: `Please provide title or contents for the post.`,
-    });
-  } else {
+  if (!title && !contents) responseHandler(response, false, 400, { error: `Please provide title or contents for the post.` });
+  else {
     try {
       const post = await db.findById(request.params.id);
       if (post[0])
@@ -149,33 +86,14 @@ router.put(`/:id`, async (request, response) => {
           const updateResponse = await db.update(request.params.id, request.body);
           if (updateResponse === 1) {
             const updatedPost = await db.findById(request.params.id);
-            response.status(200).json({
-              success: true,
-              updatedPost,
-            });
-          } else {
-            response.status(500).json({
-              success: false,
-              error: `The post information could not be modified.`,
-            });
-          }
+            responseHandler(response, true, 200, { updatedPost: updatedPost[0] });
+          } else responseHandler(response, false, 500, { error: `The post information could not be modified.` });
         } catch {
-          response.status(500).json({
-            success: false,
-            error: `The post information could not be modified.`,
-          });
+          responseHandler(response, false, 500, { error: `The post information could not be modified.` });
         }
-      else {
-        response.status(404).json({
-          success: false,
-          error: `The post with the specified ID does not exist.`,
-        });
-      }
+      else responseHandler(response, false, 404, { error: `The post with the specified ID does not exist.` });
     } catch {
-      response.status(500).json({
-        success: false,
-        error: `The post information could not be retrieved.`,
-      });
+      responseHandler(response, false, 500, { error: `The post information could not be retrieved.` });
     }
   }
 });
@@ -186,29 +104,13 @@ router.delete(`/:id`, async (request, response) => {
     if (post[0])
       try {
         const removeResponse = await db.remove(request.params.id);
-        console.log(removeResponse);
-        removeResponse &&
-          response.status(200).json({
-            success: true,
-            deletedPost: post,
-          });
+        removeResponse && responseHandler(response, true, 200, { deletedPost: post[0] });
       } catch {
-        response.status(500).json({
-          success: false,
-          error: `The post could not be removed.`,
-        });
+        responseHandler(response, false, 500, { error: `The post could not be removed.` });
       }
-    else {
-      response.status(404).json({
-        success: false,
-        error: `The post with the specified ID does not exist.`,
-      });
-    }
+    else responseHandler(response, false, 404, { error: `The post with the specified ID does not exist.` });
   } catch {
-    response.status(500).json({
-      success: false,
-      error: `The post information could not be retrieved.`,
-    });
+    responseHandler(response, false, 500, { error: `The post information could not be retrieved.` });
   }
 });
 
